b93248ff548f9ae04067e51382155deed633a082
Leal <101477230+leal-esx@users.noreply.github.com>
2022-03-12 19:51:07 +0000

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,8 @@
+RegisterCommand("removermaoo", function()
+    for _, v in pairs(GetGamePool('CObject')) do
+        if IsEntityAttachedToEntity(PlayerPedId(), v) then
+            SetEntityAsMissionEntity(v, true, true)
+            DeleteObject(v)
+        end
+    end
+end, false)
---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'remover props ! By: leal'
+
+client_script 'client.lua'
+
