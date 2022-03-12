40f1d407042a460d993b1349ea890c80bfe0b2c6
Leal <101477230+leal-esx@users.noreply.github.com>
2022-03-12 19:59:27 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,4 @@
-RegisterCommand("removermao", function()
+RegisterCommand("removerobjeto", function()
     for _, v in pairs(GetGamePool('CObject')) do
         if IsEntityAttachedToEntity(PlayerPedId(), v) then
             SetEntityAsMissionEntity(v, true, true)
