e1156a2f47f6064198062ad7f3f647763064e2f0
Leal <101477230+leal-esx@users.noreply.github.com>
2022-12-06 19:20:26 +0000

    Atualizar o client.lua
    Fix

--- client.lua
+++ client.lua
@@ -2,7 +2,7 @@ RegisterCommand("removerobjeto", function()
     for _, v in pairs(GetGamePool('CObject')) do
         if IsEntityAttachedToEntity(PlayerPedId(), v) then
             SetEntityAsMissionEntity(v, true, true)
-            DeleteObject(v)
+            DeleteEntity(v)
             DeleteObject(v)
         end
     end
